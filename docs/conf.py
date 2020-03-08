29a5783572f8d628d0fdd07abc51a3b510db0375
nraw <marsic.andrej@gmail.com>
2020-03-08 02:34:53 +0100

    Bump version: 0.0.2 → 0.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = 'kedro-argo'
 year = '2020'
 author = 'Andrej Marsic'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.2'
+version = release = '0.0.3'
 
 pygments_style = 'trac'
 templates_path = ['.']
